ce3d42b191490cd972c2b445de57d1a162062380
Choi Junyoung <nueeeng@gmail.com>
2023-05-08 20:29:29 +0900

    comment model

--- owners/models.py
+++ owners/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class OwnerComment(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # pr? = models.ForeignKey(PR?, on_delete=models.CASCADE)
+    # updated_at 처리는 어떻게?
+    content = models.TextField()
+
+    def __str__(self):
+        return str(self.content)
