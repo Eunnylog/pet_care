0ec8b0a1bcb9cc6eeccb0fb67792ccaeaa40b783
Choi Junyoung <nueeeng@gmail.com>
2023-05-09 11:15:30 +0900

    updated and comment model

--- sitters/admin.py
+++ sitters/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from owners.models import PetSitter, PetSitterComment
+from sitters.models import PetSitter, PetSitterComment
 
 admin.site.register(PetSitter)
 admin.site.register(PetSitterComment)
