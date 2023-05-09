cb455f45ad36a57793d0d80d59ab85ade516f981
Choi Junyoung <nueeeng@gmail.com>
2023-05-09 11:27:18 +0900

    comment admin display

--- owners/admin.py
+++ owners/admin.py
@@ -3,3 +3,8 @@ from owners.models import PetOwner, PetOwnerComment
 
 admin.site.register(PetOwner)
 admin.site.register(PetOwnerComment)
+
+class PetOwnerCommentDisplay(admin.ModelAdmin):
+    list_display = ('writer','owner','content','created_at','updated_at')
+    fields =('writer','owner','content','created_at','updated_at')
+    readonly_fields = ('created_at','updated_at')
--- sitters/admin.py
+++ sitters/admin.py
@@ -3,3 +3,9 @@ from sitters.models import PetSitter, PetSitterComment
 
 admin.site.register(PetSitter)
 admin.site.register(PetSitterComment)
+
+
+class PetSitterCommentDisplay(admin.ModelAdmin):
+    list_display = ('writer','sitter','content','created_at','updated_at')
+    fields =('writer','sitter','content','created_at','updated_at')
+    readonly_fields = ('created_at','updated_at')
