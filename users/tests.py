6320ff787ee87c5f28e874ffeb1562d903e7453c
heyrin1998 <qq5200@naver.com>
2023-05-09 15:42:17 +0900

    리뷰 뷰 추가

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from users.models import User
 
-# Create your tests here.
+# class ReviewCreateTest(APITestCase):
+#     def setUp(self):
+#         self.review_data = {'contet':'테스트','star':5}
+#         self.user = User.objects.create_user('hello','hello@hi.com','hello')
+        
+#     def test_create_review(self):
+#         response = self.client.post(
+#             path = reverse("petownerreview_view", kwargs={"user_id":1}),
+#             data = self.review_data,
+#         )
+#         self.assertEqual(response.status_code, 201)
