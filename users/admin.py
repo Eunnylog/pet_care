06d4622454cd5a257e11c917954a6da7e827036c
Eunnylog <lllala0710@gmail.com>
2023-05-11 22:10:52 +0900

    CheckEmailAdmin

--- users/admin.py
+++ users/admin.py
@@ -95,18 +95,10 @@ class UserAdmin(BaseUserAdmin):
     filter_horizontal = []
 
 
-class CommonDisplayAdmin(admin.ModelAdmin):
-    fields=()
-    list_display=()
-    readonly_fields=()
-    common_list_display=('created_at','updated_at',"show_status")
-    common_fields =('created_at','updated_at',"show_status")
-    common_readonly_fields = ('created_at','updated_at')
-    def __init__(self, model: type, admin_site):
-        self.fields+=self.common_fields
-        self.list_display+=self.common_list_display
-        self.readonly_fields+=self.common_readonly_fields
-        super().__init__(model, admin_site)
+class CheckEmailAdmin(admin.ModelAdmin):
+    list_display=('email','random_num','try_num','created_at')
+    list_display=('email','random_num','try_num','created_at')
+    readonly_fields=('created_at',)
 
 class PetOwnerReviewDisplay(CommonDisplayAdmin):
     fields=('writer','owner','content','star')
